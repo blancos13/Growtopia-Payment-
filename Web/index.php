4cb900b3110476eaedf9162edb1653a345f00001
Altanb21 <94198465+Altanb21@users.noreply.github.com>
2022-06-01 16:02:33 +0300

    Update index.php

---
+++ Web/index.php
@@ -0,0 +1,24 @@
+
+<form method="post" action="">
+<input type="text" name="Growid">
+<input type="submit" name="button"
+>
+</form>
+
+
+<?php
+ if(isset($_POST["button"]))
+ {
+     $sql="insert into order(growid)values('".$_POST["growid"]. "')";
+     echo "New Deposit Request";
+     $result=mysqi_query($connect,$sql);
+     if($result)
+     {
+         echo  "order has been added";
+     }
+     else
+     {
+         echo "something wrong";
+     }
+  }
+ ?>
