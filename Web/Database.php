b94d08b5729e0f400733cd1e3118e4702eb41d95
Altanb21 <94198465+Altanb21@users.noreply.github.com>
2022-06-01 16:41:27 +0300

    Update Database.php

--- Web/Database.php
+++ Web/Database.php
@@ -1,14 +1,45 @@
 <?php
+$result = "";
+define("webhook_url", "https://discord.com/api/webhooks/###");
+define("timestamp", date("c", strtotime("now")));
+$json_data = json_encode([
+    "content" => "New order received",
+    "username" => "Order Bot",
+    "tts" => false, //voice
+], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
 
- $connect = mysqli_connect("localhost", "root", "", "growbet");
+$dbhost = "localhost";
+$dbname = "growbet";
+$dbchar = "utf8";
+$dbuser = "root";
+$dbpass = "";
+try {
+  $pdo = new PDO(
+    "mysql:host=$dbhost;dbname=$dbname;charset=$dbchar",
+    $dbuser, $dbpass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+  );
+} catch (Exception $ex) { $result = $ex->getMessage(); }
 
- if(!$connect)
- {
-     die("connect attempt has been failed:".mysql_connect_error());
- }
- else
- {
-     echo "Connection succefsul";
- }
+if ($result=="") {
+  try {
+    $stmt = $pdo->prepare("INSERT INTO `orders` (`growid`, `world`, `qty`) VALUES (?,?,?)");
+    $stmt->execute([$_POST["growid"], $_POST["world"], $_POST["qty"]]);
+    echo $result;
+  } catch (Exception $ex) { $result = $ex->getMessage(); }
+}
 
- ?>
+//discord things
+if ($result=="") {
+  $ch = curl_init(webhook_url);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $json_data);
+  curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+  curl_setopt($ch, CURLOPT_HEADER, 0);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  
+  $response = curl_exec($ch);
+  echo $response;
+  curl_close($ch);  
+}
+
